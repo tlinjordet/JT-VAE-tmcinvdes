bb366d2a22fe7d9695355d04fe51209b46da06aa
Strandgaard96 <magnus.str@gmail.com>
2024-06-16 11:43:04 +0200

    removed old implementation

--- old/molvae/jtnn/__init__.py
+++
@@ -1,7 +0,0 @@
-from .chemutils import decode_stereo
-from .datautils import MoleculeDataset, PropDataset
-from .jtnn_vae import JTNNVAE
-from .jtprop_vae import JTPropVAE
-from .mol_tree import MolTree, Vocab
-from .mpn import MPN, mol2graph
-from .nnutils import create_var
